671b85df0e75041b5cd1a91e24d1ff0e531310fb
Abiyasa <abiyasa.hendro@nri.co.id>
2024-03-22 10:31:18 +0700

    feature(neovim): add catpuccin

--- roles/neovim/plugins/catpuccin.lua
+++ roles/neovim/plugins/catpuccin.lua
@@ -1,8 +1,9 @@
 return {
+	{ "catpuccin/nvim", name = "catppuccin", priority = 1000 },
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "catppuccin",
+			colorscheme = "catppuccin-mocha",
 		},
 	},
 }
