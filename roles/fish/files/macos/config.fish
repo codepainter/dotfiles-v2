cea5e99cb346f9d54d377ca9467cfadef0c09176
Abiyasa <abiyasa.hendro@nri.co.id>
2024-04-04 10:08:04 +0700

    fix(fish): add macos config

---
+++ roles/fish/files/macos/config.fish
@@ -0,0 +1,13 @@
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    source /home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.fish
+    fish_ssh_agent
+end
+
+source /home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.fish
+
+starship init fish | source
+
+setenv EDITOR nvim
