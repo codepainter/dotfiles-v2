97383c56fd0030bfd8d108d0caf168e370054f7d
Abiyasa <abiyasa2410@gmail.com>
2024-04-08 11:59:45 +0700

    chore: delete unused file

--- automation/destination.py
+++
@@ -1,6 +0,0 @@
-# Description: This file contains the destination paths for the files to be moved.
-folder_map= {
-    "[Kusayarou]": "/Volumes/home/Temporarily/[Kusayarou]",
-    "[Takashima]": "/Volumes/home/Temporarily/[Takashima]",
-    "[Pink Gate (Remu)]": "/Volumes/home/Temporarily/[Pink Gate (Remu)]",
-}
